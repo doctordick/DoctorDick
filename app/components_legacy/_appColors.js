206b135fd54690e2c4f99848d8a3bf326919889f
henrywongworking@gmail.com <henryw4k@gmail.com>
2016-08-14 17:28:01 -0700

    Setup application structure

---
+++ app/components_legacy/_appColors.js
@@ -0,0 +1,19 @@
+import colors from './_colors'
+
+const appColors = {
+  backgroundColorPrimary: colors.blue_4,
+  backgroundColorCard: colors.blue_2,
+  backgroundColorMain: colors.white,
+  backgroundColorQuestion: colors.blue_2,
+  backgroundColorMenuItem: colors.blue_2,
+  borderColorSetup: colors.blue_5,
+  borderColorNext: colors.blue_5,
+  borderColorHeader: colors.blue_1,
+  borderColorLocationButton: colors.blue_3,
+  buttonColorMain: colors.green_1,
+  buttonColorNext: colors.blue_3,
+  textGrey: colors.grey_1,
+  defaultTextColor: colors.white
+};
+
+export default appColors
---
+++ app/components_legacy/_colors.js
@@ -0,0 +1,13 @@
+const colors = {
+	black: '#000000',
+	blue_1: '#0785ab',
+	blue_2: '#35A5C2',
+	blue_3: '#0483ad',
+	blue_4: '#0C94B9',
+	blue_5: '#19c3f5',
+	white: '#ffffff',
+	green_1: '#00bbbb',
+	grey_1: '#777777'
+};
+
+export default colors
