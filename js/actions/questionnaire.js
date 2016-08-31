d076ac6c902918bc9cba3b6bbd3fd88c169fc1e6
David Ernst <git@dsernst.com>
2016-08-31 14:20:43 -0700

    fix typo in questionnaire.js

--- js/actions/questionnaire.js
+++ js/actions/questionnaire.js
@@ -4,7 +4,7 @@ import type {Action, RecommendationCode} from './types'
 
 export const REACH_DECISION = "REACH_DECISION";
 
-export function reachDecision(decision:RecommendationCodeΩ, questionnaireType:string):Action {
+export function reachDecision(decision:RecommendationCode, questionnaireType:string):Action {
   return {
     type: REACH_DECISION,
     decision,
