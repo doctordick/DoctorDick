cf5044e31a09c9274607e54c23e0a84ceafb3640
Hou Chia <hou@styleseat.com>
2016-09-06 15:46:31 -0700

    Add Care Locator button to 'contactDoc' scenario

--- js/components/recommendation-page/recommendation-card/recommendations.js
+++ js/components/recommendation-page/recommendation-card/recommendations.js
@@ -1,29 +1,29 @@
 export default {
 
   'testRec': {
-    text: 'You should get tested for HIV.',
+    text: 'You should get tested for HIV. Talk to your doctor or other healthcare provider if you have any questions about HIV treatment or about your health in general. You can also use Doctor Dick\'s HIV care locator service to find HIV testing sites nearby.',
     findTestingCenter: true,
     setReminders: true,
     orderHomeKit: true,
   },
 
   'referCare': {
-    text: 'You should see a doctor or other healthcare provider about HIV treatment.',
+    text: 'You should see a doctor or other healthcare provider about HIV treatment. You can use Doctor Dick\'s HIV care locator service to find HIV testing sites nearby.',
     findTestingCenter: true,
     setReminders: false,
     orderHomeKit: false,
   },
 
   'followupRec': {
-    text: 'An HIV test is recommended every three months for many guys, but some guys might want or need to get tested more frequently. Talk to your doctor or other healthcare provider if you have questions about how frequently you should get tested, or whether you need another HIV test now.',
+    text: 'An HIV test is recommended every three months for many guys, but some guys might want or need to get tested more frequently. Talk to your doctor or other healthcare provider if you have questions about how frequently you should get tested, or whether you need another HIV test now. You can also use Doctor Dick\'s HIV care locator service to find HIV testing sites nearby.',
     findTestingCenter: true,
     setReminders: true,
     orderHomeKit: false,
   },
 
   'contactDoc': {
-    text: 'You should keep seeing your doctor for HIV care.\nOr use the HIV care locator service to find other HIV doctors.',
-    findTestingCenter: false,
+    text: 'You should keep seeing your doctor for HIV care.\nOr use the HIV care locator service to find other HIV testing sites nearby.',
+    findTestingCenter: true,
     setReminders: false,
     orderHomeKit: false,
   },
--- js/components/survey/results.js
+++ js/components/survey/results.js
@@ -16,7 +16,7 @@ export class TestRec extends Component {
         return (
             <View style={styles.testRec}>
                 <Text style={{textAlign: 'center', color: '#696969', fontSize: 16, marginBottom: 20}}>
-                    You should get tested for HIV.
+                    You should get tested for HIV. Talk to your doctor or other healthcare provider if you have any questions about HIV treatment or about your health in general. You can also use Doctor Dick's HIV Testing Locator to find HIV testing sites nearby.
                 </Text>
                 <Button block success style={{ marginBottom: 20, marginLeft: 20, marginRight: 20, borderRadius: 0, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} onPress={this.props.pushNewRoute.bind(this, 'careLocator')}>
                     <Icon style={{color: 'white'}} name='ios-pin-outline' />
@@ -69,7 +69,7 @@ export class FollowupRec extends Component {
         return (
             <View style={styles.testRec}>
                 <Text style={{textAlign: 'center', color: '#696969', fontSize: 16, marginBottom: 20, paddingLeft: 20, paddingRight: 20}}>
-                    An HIV test is recommended every three months for many guys, but some guys might want or need to get tested more frequently. Talk to your doctor or other healthcare provider if you have questions about how frequently you should get tested, or whether you need another HIV test now. 
+                    An HIV test is recommended every three months for many guys, but some guys might want or need to get tested more frequently. Talk to your doctor or other healthcare provider if you have questions about how frequently you should get tested, or whether you need another HIV test now. You can also use Doctor Dick's HIV Testing Locator to find HIV testing sites nearby.
                 </Text>
                 <Button block success style={{ marginBottom: 20, marginLeft: 20, marginRight: 20, borderRadius: 0, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} onPress={this.props.pushNewRoute.bind(this, 'careLocator')}>
                     <Icon style={{color: 'white'}} name='ios-pin-outline' />
@@ -95,9 +95,15 @@ export class ContactDoc extends Component {
     render() {
         return (
             <View style={styles.testRec}>
-                <Text style={{textAlign: 'center', color: '#696969', textAlign: 'center', paddingLeft: 10, paddingRight: 10}}>
-                    You said you are seeing a doctor or other healthcare provider for HIV treatment. That’s great. {"\n"} You should contact your doctor or other healthcare provider if you have any questions about HIV treatment or about your health in general.
+                <Text style={{textAlign: 'center', color: '#696969', paddingLeft: 10, paddingRight: 10}}>
+                    You said you are seeing a doctor or other healthcare provider for HIV treatment. that’s great. {"\n"} Talk to your doctor or other healthcare provider if you have any questions about HIV treatment or about your health in general. You can also use Doctor Dick's HIV Testing Locator to find HIV testing sites nearby.
                 </Text>
+                <Button block success style={{ marginTop: 20, marginBottom: 20, marginLeft: 20, marginRight: 20, borderRadius: 0, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} onPress={this.props.pushNewRoute.bind(this, 'careLocator')}>
+                    <Icon style={{color: 'white'}} name='ios-pin-outline' />
+                    <Text style={{color: 'white'}}>
+                        Find a Testing Center
+                    </Text>
+                </Button>
                 <Button transparent textStyle={{color: '#0000EE', fontSize: 14}} style={{alignSelf: 'center'}} onPress={this.props.openLink.bind(this, 'https://www.aids.gov/hiv-aids-basics')}>
                     More Information About HIV
                 </Button>
