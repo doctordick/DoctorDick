e53565f42db8458f434d9ae88042a43a62c7b530
kchia <kchia87@gmail.com>
2016-09-24 23:56:21 -0700

    Increase width of info icon

--- js/components/care-locator/index.js
+++ js/components/care-locator/index.js
@@ -89,7 +89,7 @@ class CareLocator extends Component {
                                     leftCalloutView: <Icon name='ios-navigate-outline' style={{color:'black'}} onPress={() => Linking.openURL(
                                         'http://maps.apple.com/?q='+ center.title+', '+center.streetAddress+', '+center.postalCode
                                     )} />,
-                                    rightCalloutView: <Icon name='ios-information-outline' style={{color: 'black', fontSize: 40}} onPress={() => Linking.openURL(center.link)} />
+                                    rightCalloutView: <Icon name='ios-information' style={{color: 'black', fontSize: 40, width: 15}} onPress={() => Linking.openURL(center.link)} />
                                 })))()}
                             />
                         </View>
